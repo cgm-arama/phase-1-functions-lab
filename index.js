7d150e3d5fa56491ac9c25706d25a2716ad1cd15
Eric <eric.arama@student.moringaschool.com>
2023-03-20 17:44:29 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(num1, num2 = 42) {
+    if (num1 > num2) {
+return num1 - num2;
+ } else {
+    return num2 - num1;
+ }
+}
+console.log(distanceFromHqInBlocks())
+
+function distanceFromHqInFeet(num1, num2 ,num3 = 264) {
+    distanceFromHqInBlocks(num1, num2 = 42);
+    if (num1 > num2) {
+return (num1 - num2)*num3;
+ } else {
+    return (num2 - num1)*num3;
+ }
+}
+console.log(distanceFromHqInFeet())
+
+function distanceTravelledInFeet(start, destination) {
+    if (start < destination) {
+        return (destination - start)*264;
+    } else {
+        return (start-destination)*264;
+    }
+}
+console.log(distanceTravelledInFeet())
+
+function calculatesFarePrice(start , destination){
+    if((destination - start) <= 400){
+        return 0;
+    }else if ((destination - start) < 2000){
+        return (destination - start)*0.02;
+    }else if ((destination - start) < 2500){
+        return 25;
+    }else if((destination - start) > 2500){
+        return 'cannot travel that far';
+    }
+}
+console.log(calculatesFarePrice(0 , 700))
+
+
+
+
+
+
+
+
